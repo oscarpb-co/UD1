3d1df01fc518f653db502c461c10db6f7fb4e99d
Oscar <oscarperez.piringalla@gmail.com>
2025-10-01 10:46:44 +0200

    Se añade el ejercicio 42 por que no estaba hecho y hay sospechas fundadas de que caiga en el examen

---
+++ src/ejercicio/pkg1/Ejercicio42.java
@@ -0,0 +1,10 @@
+package ejercicio.pkg1;
+
+public class Ejercicio42 {
+
+    public static void main(String[] args) {
+       //No esta hecho en clase e igual al profe le da por ponerlo 
+       //en el examen.
+    }
+
+}
